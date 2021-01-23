cf23cc280a8f96c5da47fc30aaaac0be9553c841
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2021-01-23 00:26:17 -0500

    added parallax.js and fonts

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 
 $(document).ready(function() {
 
-    $("#scroll-down").fadeIn(2000);
-    $("#scroll-down").fadeOut(2000);
-    $("#scroll-down").fadeIn(2000);
-    $("#scroll-down").fadeOut(2000);
-    $("#scroll-down").fadeIn(2000);
-    $("#scroll-down").fadeOut(2000);
-    $("#scroll-down").fadeIn(2000);
-    $("#scroll-down").fadeOut(2000);
-    $("#scroll-down").fadeIn(2000);
-    $("#scroll-down").fadeOut(2000);
-    $("#scroll-down").fadeIn(2000);
-    $("#scroll-down").fadeOut(2000);
     
 });
 
