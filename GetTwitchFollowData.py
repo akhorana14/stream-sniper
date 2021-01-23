b23cb23a434e6c338c379909ccab422b66dc9ab5
JeongB-L <lee3546@purdue.edu>
2021-01-23 15:24:21 -0500

    playingshit file

--- GetTwitchFollowData.py
+++ GetTwitchFollowData.py
@@ -1,7 +1,7 @@
 from twitch import TwitchClient
 
 client = TwitchClient('f0ilugz4jjhaheg2ioky2oe0wif8oo')
-users = client.users.translate_usernames_to_ids(['xsmartmc'])
+users = client.users.translate_usernames_to_ids(['xsmart'])
 
 for user in users:
      follows = client.users.get_follows(user_id=user.id, limit=100, offset=0)
